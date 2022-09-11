600d724a67f3f75ea1f9124256d39f4bb9e97913
pat <pat.escalona7@gmail.com>
2022-09-11 15:26:44 -0500

    Function to randomly choose one outcome is working.

--- rps.js
+++ rps.js
@@ -1,2 +1,12 @@
 /* rock paper scissors javascript file */
 
+console.log("Hello, world!");
+
+const myArray = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let computerChoice = myArray[Math.floor(Math.random() * myArray.length)];
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
