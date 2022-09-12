112d5942d7304dbaa99f68724e0d11928e11b964
Pat Escalona <pat.escalona7@gmail.com>
2022-09-11 20:26:02 -0500

    Comparison of player and computer choices not working correctly. Returns undefined game result.

--- rps.js
+++ rps.js
@@ -1,12 +1,79 @@
 /* rock paper scissors javascript file */
 
+// rock > scissors
+// paper > rock
+// scissors > paper
+
 console.log("Hello, world!");
 
 const myArray = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
+function computerSelection() {
+    console.log("in the computerSelection function");
     let computerChoice = myArray[Math.floor(Math.random() * myArray.length)];
+    console.log(computerChoice);
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function playerSelection() {
+    let playerChoice = prompt("Enter rock, paper, or scissors", "rock, paper, or scissors");
+    console.log(playerChoice);
+    return playerChoice;
+}
+
+
+function playRound(playerChoice, computerChoice) {
+    console.log("in the playRound function");
+    let gameResult;
+    // player enters choice for round
+
+    // handle a tie game
+    if (playerSelection == computerSelection) {
+        gameResult = "tie";
+        console.log("game is a tie");
+        return gameResult;
+    }
+    // handle all other cases
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection == "scissors") {
+                gameResult = "win";
+            } else {
+                gameResult = "lost"
+            }
+            return gameResult;
+        case "paper":
+            if (computerSelection == "rock") {
+                gameResult = "win";
+            } else {
+                gameResult = "lost"
+            }
+            return gameResult;
+        case "scissors":
+            if (computerSelection == "paper") {
+                gameResult = "win";
+            } else {
+                gameResult = "lost"
+            }
+            return gameResult;
+    }
+}
+
+function game() {
+    /* call playRound() here and play a 
+    5 round game that keeps score and reports a winner or loser
+    at the end */
+    let i = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+    for (i = 0; i < 5; i++) {
+        console.log(i);
+        console.log("Game result is " + playRound(playerSelection(), computerSelection()));
+    }
+
+}
+
+
+
+// let playerSelection = "rock";
+console.log(game());
